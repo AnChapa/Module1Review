6fa3b19717d811e70616f73fc3489feda32194ff
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-16 15:58:08 -0600

    User input and Bonus

--- Module1Review/Program.cs
+++ Module1Review/Program.cs
@@ -4,8 +4,39 @@
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What shape do you want?\nChoose either a Circle, Triangle, Rectangle, or Square:");
+            var shape = Console.ReadLine().ToLower();
+            switch (shape)
+            {
+                case "circle":
+                    Console.WriteLine("\nWhat is the radius:");
+                    double radius = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"\nThe area of a circle with {radius} radius is: {CircleArea(radius)}");
+                    break;
+                case "triangle":
+                    Console.WriteLine("\nWhat is the length of the base for the triangle:");
+                    double baseLength = double.Parse(Console.ReadLine());
+                    Console.WriteLine("\n What is the height of the triangle:");
+                    double height = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"\nThe area of a triangle with a {baseLength} base and {height} height is: {TriangleArea(baseLength, height)}");
+                    break;
+                case "rectangle":
+                    Console.WriteLine("\nWhat is the length of the rectangle:");
+                    double length = double.Parse(Console.ReadLine());
+                    Console.WriteLine("\nWhat is the width of the rectangle:");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"\nThe area of a rectangle with a length of {length} and width {width} is: {RectangleArea(length, width)}");
+                    break;
+                case "square":
+                    Console.WriteLine("\nWhat is the length of the sides for the square:");
+                    double side = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"\nThe area of a square with sides of lenght {side} is: {SquareArea(side)}");
+                    break;
+                default:
+                    Console.WriteLine($"\n{shape} is not a valid shape.");
+                    break;
+            }
+
         }
 
         public static double CircleArea(double radius)
--- Module1Review/obj/Debug/net6.0/Module1Review.AssemblyInfo.cs
+++ Module1Review/obj/Debug/net6.0/Module1Review.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840ee57af06e7c9f8eacf6abf33e69e4d4c2847c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
